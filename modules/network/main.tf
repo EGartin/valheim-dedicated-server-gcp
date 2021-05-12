0a80e4225dde6d175774a684de1486ff9e2c71fd
Elijah Gartin <elijah.gartin@rsa.com>
2021-05-11 19:00:26 -0600

    Updated Network module to only build one subnet and modified provider to not use impersonation.

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -7,15 +7,16 @@
 
 #RANGE 10.10.x.x VNET NETWORK
 resource "google_compute_network" "valheim-vpc" {
-  name                = "valheim-vnet"
-  description         = "valheim-vpc"
+  name                    = "valheim-vnet"
+  description             = "valheim-vpc"
+  auto_create_subnetworks = false
   #address_space       = ["10.10.0.0/16"]
 }
 
 #10 Subnet
 resource "google_compute_subnetwork" "subnet" {
-  name                = "valheim-subnet"
-  ip_cidr_range        = "10.10.10.0/24"
-  region              = var.region
-  network             = google_compute_network.valheim-vpc.id
+  name                    = "valheim-subnet"
+  ip_cidr_range           = "10.10.10.0/24"
+  region                  = var.region
+  network                 = google_compute_network.valheim-vpc.id
 }
